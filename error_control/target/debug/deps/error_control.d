5cdd996c30b21e93d03f1f720d53ce3529304f71
remote <meyoub612@naver.com>
2023-10-30 23:30:29 +0900

    error controll

---
+++ error_control/target/debug/deps/error_control.d
@@ -0,0 +1,5 @@
+C:\Users\ejatk\RustroverProjects\LeaningRust\error_control\target\debug\deps\error_control.exe: src\main.rs
+
+C:\Users\ejatk\RustroverProjects\LeaningRust\error_control\target\debug\deps\error_control.d: src\main.rs
+
+src\main.rs:
---
+++ error_control/target/debug/error_control.d
@@ -0,0 +1,1 @@
+C:\Users\ejatk\RustroverProjects\LeaningRust\error_control\target\debug\error_control.exe: C:\Users\ejatk\RustroverProjects\LeaningRust\error_control\src\main.rs
